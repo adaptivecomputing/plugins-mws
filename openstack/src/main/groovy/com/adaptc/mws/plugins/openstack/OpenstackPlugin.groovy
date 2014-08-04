809d3fce5a8030b4895742ea5fa1b66f8f1a4b80
Brian Saville <bsaville@adaptivecomputing.com>
2014-08-04 12:51:20 -0600

    AC-7561 Create vapor VM from MWS plugin

--- openstack/src/main/groovy/com/adaptc/mws/plugins/openstack/OpenstackPlugin.groovy
+++ openstack/src/main/groovy/com/adaptc/mws/plugins/openstack/OpenstackPlugin.groovy
@@ -1,7 +1,10 @@
 package com.adaptc.mws.plugins.openstack
 
 import com.adaptc.mws.plugins.*
+import org.openstack4j.api.Builders
 import org.openstack4j.api.OSClient
+import org.openstack4j.model.compute.Server
+import org.openstack4j.model.compute.ServerCreate
 import org.openstack4j.openstack.OSFactory
 
 class OpenstackPlugin extends AbstractPlugin {
@@ -18,6 +21,30 @@ class OpenstackPlugin extends AbstractPlugin {
 				.credentials("someone", "somepass")
 				.tenantName("Tenant1")
 				.authenticate()
-		return [servers:osClient.compute().servers().list()*.name]
+		def flavorId = osClient.compute().flavors().list().find { it.name=="flavor-name" }.id
+		def imageId = osClient.compute().images().list().find { it.name=="image-name" }.id
+		log.debug("Using flavor ${flavorId} and image ${imageId}")
+		ServerCreate newServer = Builders.server()
+				.name("burst-vm-${new Date().time}")
+				.flavor(flavorId)
+				.image(imageId)
+				.build()
+		Server server = osClient.compute().servers().boot(newServer)
+		def startDate = new Date()
+		while(server.status!=Server.Status.ACTIVE) {
+			if ((new Date().time - startDate.time) > 30000) {
+				log.error("Could not get information from server, took too long to get IP")
+				break
+			}
+			// Else keep trying
+			sleep(1000)
+			server = osClient.compute().servers().get(server.id)
+		}
+		return [
+				id:server.id,
+		        ipAddress:server.getAddresses().getAddresses()['vlan-name'].first().addr,
+				name:server.name,
+				powerState:server.powerState
+		]
 	}
 }
